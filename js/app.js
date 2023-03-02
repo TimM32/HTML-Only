a5af1dd8fce7bc2799404634303b7dc259818ad3
TimM32 <timmaupin@live.com>
2023-03-02 13:27:38 -0600

    add console log for movie question

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ console.log("user name: ", userName);
 let confirmMessage = confirm("Do you want to continue?");
 console.log("confirm message: ", confirmMessage);
 let userMovie = prompt("What's your favorite movie?");
+console.log()
 let lowerCaseMovie = userMovie.toLowerCase();
 
 
