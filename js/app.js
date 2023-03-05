ae10d35a738ab08908b5c172d4ba69dd1bb080d3
Nic Barrett <nrbarrett@businessolver.com>
2023-03-05 13:24:49 -0600

    remove movie name log

--- js/app.js
+++ js/app.js
@@ -6,25 +6,23 @@ let userName = prompt("What is your name?");
 console.log("user name: ", userName);
 let confirmMessage = confirm("Do you want to continue?");
 console.log("confirm message: ", confirmMessage);
-let userMovie = prompt("What's your favorite movie?");
-console.log()
 let lowerCaseMovie = userMovie.toLowerCase();
-
+getMovieName();
 
 function getMovieName() {
-let userMovie = prompt("what's your favorite movie?")    
-let response;
+    let userMovie = prompt("what's your favorite movie?")
+    let response;
 
-if (userMovie === "batman") {
-    response = "Awesome!";
-} else if (userMovie === "ironman") {
-    response = "Best Movie Ever!!";
-} else if (userMovie === "troy") {
-    response = "Ah a classic!";
-} else {
-    response = "Great Choice!";
-}
-console.log("response", response);
-return response;
+    if (userMovie === "batman") {
+        response = "Awesome!";
+    } else if (userMovie === "ironman") {
+        response = "Best Movie Ever!!";
+    } else if (userMovie === "troy") {
+        response = "Ah a classic!";
+    } else {
+        response = "Great Choice!";
+    }
+    console.log("response", response);
+    return response;
 }
 
